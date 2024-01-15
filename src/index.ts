9982f5a7b6e9ceb32fdd0c707d14977fb99356cb
Ilia Anokhin <speeddekeron@mail.ru>
2024-01-15 16:50:26 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+const data = {
+  persons: [
+    {id: 1, title: 'warm'},
+    {id: 2, title: 'cold'}
+  ]
+}
+
+app.get('/', (req, res) => {
+  res.send('hell')
+})
+app.get('/home', (req, res) => {
+  let foundQuery = data.persons
+
+  if (req.query.title) {
+    foundQuery = foundQuery
+      .filter(c => c.title.includes(req.query.title as string))
+  }
+
+  res
+    .status(200)
+    .json(foundQuery)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
